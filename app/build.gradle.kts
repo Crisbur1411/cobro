c8e0dbd20b57833c117da6bace1932156cc4feb1
ElPaste07 <pastelinisaias@gmail.com>
2025-05-22 19:27:49 -0600

    cambios en la interfaz de precarga cambiado logotipo y colores

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     implementation ("com.google.code.gson:gson:2.10.1")
     implementation ("com.google.android.material:material:1.11.0")
 
+
+
     implementation ("com.google.android.material:material:1.9.0")
     implementation(libs.appcompat)
     implementation(libs.material)
